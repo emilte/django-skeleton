fda10f218e36bb875e64afab60a515b748a69852
Emil Telstad <emil.telstad@gmail.com>
2021-10-30 22:15:15 +0200

    Update root.commands

--- root/management/commands/deletemigrations.py
+++ root/management/commands/deletemigrations.py
@@ -4,9 +4,7 @@ import glob
 import shutil
 
 from django.conf import settings
-from django.core import management
 from django.core.management.base import BaseCommand
-
 # End: imports -----------------------------------------------------------------
 
 
@@ -24,7 +22,7 @@ class Command(BaseCommand):
                 for f in app_migrations:
                     os.remove(f)
                     print(f"Removed {f}")
-                    
+
                 pycache = f"{migrations}/__pycache__"
                 shutil.rmtree(pycache)
                 print(f"Removed {pycache}")
--- root/management/commands/setupmigrations2.py
+++ root/management/commands/setupmigrations2.py
@@ -1,11 +1,7 @@
 # imports
-import os
-import glob
-
 from django.conf import settings
 from django.core import management
 from django.core.management.base import BaseCommand
-
 # End: imports -----------------------------------------------------------------
 
 
@@ -13,9 +9,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for app in settings.INSTALLED_APPS:
-            
+
             appname = app.split('.')[-1]
-            
+
             try:
                 management.call_command('makemigrations', appname)
 
