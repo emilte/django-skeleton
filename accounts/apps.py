8f3a7201cded5efc9894a16fcabfb5a4683448c7
Emil Telstad <emil.telstad@gmail.com>
2021-11-24 00:04:24 +0100

    Add pylint-quotes

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,4 +1,6 @@
 from django.apps import AppConfig
+from django.core import management
+from django.core.management.base import CommandError
 
 
 class AccountsConfig(AppConfig):
@@ -6,3 +8,10 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         import accounts.signals  # pylint: disable=unused-import import-outside-toplevel
+
+        try:
+            # define credentials in '.env' file
+            management.call_command('createsuperuser', interactive=False)
+        except CommandError as e:
+            # multiple calls of 'createsuperuser' will raise exception because the username is already taken
+            print(__file__, e)
